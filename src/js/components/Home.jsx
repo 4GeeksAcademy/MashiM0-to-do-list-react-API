3e09f3de5c2b6f2b40d289e3df35c2340f57d01b
MashiM0 <massimog.boschettib@gmail.com>
2025-03-11 18:44:51 +0000

    [create to-do list component]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let nextId = 0;
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [task, setTask] = useState("")
+	const [tasks, setTasks] = useState([]);
+	const [style, setStyle] = useState({display: 'none'});
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="d-flex justify-content-center">
+			<div className="text-center taskpad m-5 card p-2">
+				<div className="row px-3 d-flex justify-content-between">
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+					<div className="hole px-1 mx-1" />
+				</div>
+				<h1 className="mt-3 fw-bold mb-0">To-do</h1>
+				<div className="row p-3">
+					<input className="col-9" label="What the dog doin" type="text" value={task} onChange={e => setTask(e.target.value)} />
+					<button className="col ms-2 btn btn-info fw-bold text-light" onClick={() => {
+						setTasks([
+							...tasks,
+							{ id: nextId++, name: task }
+						]);
+					}}>
+						Add
+					</button>
+				</div>
+				<div className="border-bottom p-1 mb-3" />
+				<div className="tasks">
+					<ul className="list-group p-1">
+						{tasks.map(x => (
+							<li className="list-group-item d-flex justify-content-between" key={x.id} style={{height:"56px"}} onMouseEnter={e => {
+								setStyle({display: 'block'});
+							}}
+							onMouseLeave={e => {
+								setStyle({display: 'none'})
+							}}
+					   >
+								<p className="text-primary-emphasis my-auto">{x.name}</p>
+												<button className="btn btn-danger" style={style} onClick={() => {
+									setTasks(
+										tasks.filter(a =>
+											a.id !== x.id
+										)
+									);
+								}}>
+									X
+								</button>
+							</li>
+							
+						))}
+					</ul>
+					<div className="border-bottom p-1 mb-2" />
+					<div className="text-body-tertiary"> There are {tasks.length == 0 ? "no" : tasks.length} tasks left. {tasks.length == 0 ? "Good Job!" : " "}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
